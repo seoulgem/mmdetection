022c013d22f12b88632112fa04647347063407e7
seoulgem <speedwall@nate.com>
2020-11-04 05:20:36 +0000

    init

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,4 +1,4 @@
-checkpoint_config = dict(interval=5)
+checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
     interval=50,
@@ -9,8 +9,7 @@ log_config = dict(
 # yapf:enable
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
-#'/content/mmdetection/htc_r50_fpn_1x_coco_20200317-7332cf16.pth'
+load_from = '/content/mmdetection/htc_r50_fpn_1x_coco_20200317-7332cf16.pth'
 resume_from = None
 workflow = [('train', 1)]
 work_dir='/content/gdrive/My Drive/k-fashion/model/htc_without_semantic_r50_fpn_1x/'
