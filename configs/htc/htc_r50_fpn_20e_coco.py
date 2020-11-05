91e412de9de62901984d62c025b3ee6fe1a1edb9
seoulgem <73836158+seoulgem@users.noreply.github.com>
2020-11-05 23:05:44 +0900

    Update htc_r50_fpn_20e_coco.py

--- configs/htc/htc_r50_fpn_20e_coco.py
+++ configs/htc/htc_r50_fpn_20e_coco.py
@@ -1,5 +1,5 @@
 #_base_ = './htc_r50_fpn_1x_coco.py'
-_base_ = './htc_without_semantic_r50_fpn_1x_coco'
+_base_ = './htc_without_semantic_r50_fpn_1x_coco.py'
 # learning policy
 lr_config = dict(step=[16, 19])
 total_epochs = 20
